b90569a7734b38396c9ecae345b14020802a757a
Jorge:Calderon <calderon.rjorge@gmail.com>
2023-05-15 16:02:03 -0400

    015-Funciones con Objetos como argumentos

--- 01-typescript-intro/src/topics/03-functions.ts
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -3,20 +3,42 @@
 function addNumbers(a: number, b: number): number {
   return a + b
 }
-const result: number = addNumbers(1, 2);
+// const result: number = addNumbers(1, 2);
 
 // TIP: funcion flecha
 const addNumbersArrow = (a: number, b: number): string => {
   return `${a + b}`;
 }
-const result2: string = addNumbersArrow(1, 2)
+// const result2: string = addNumbersArrow(1, 2)
 
 // TIP: funcion con parametros opcionales y pre-definidos
 function multiply(firstNumber: number, secondNumber?: number, base: number = 2) {
   return firstNumber * base
 }
-const multiplyResult: number = multiply(5);
+// const multiplyResult: number = multiply(5);
 
-console.table({ result, result2, multiplyResult });
+// console.table({ result, result2, multiplyResult });
+
+interface Character {
+  name: string;
+  hp: number;
+  showHp: () => void;
+}
+
+const healCharacter = (character: Character, amount: number) => {
+  character.hp += amount
+}
+
+const strider: Character = {
+  name: 'Strider',
+  hp: 50,
+  showHp() {
+    console.log(`Puntos de vida ${this.hp}`)
+  }
+}
+
+healCharacter(strider, 10);
+
+strider.showHp()
 
 export { }
