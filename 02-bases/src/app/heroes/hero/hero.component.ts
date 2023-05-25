7086923355ec17265e9487c484b7a779e07f225b
Jorge:Calderon <calderon.rjorge@gmail.com>
2023-05-24 21:40:23 -0400

    043-Tarea Cambiar nombre y edad

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -18,4 +18,12 @@ export class HeroComponent {
     return `${this.name} - ${this.age}`
   }
 
+  changeHero(): void {
+    this.name = 'Spider-Man';
+  }
+
+  changeAge(): void {
+    this.age = 31;
+  }
+
 }
