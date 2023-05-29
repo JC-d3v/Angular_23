56291a562046184291f3c385057344e45569569f
Jorge:Calderon <calderon.rjorge@gmail.com>
2023-05-29 15:59:46 -0400

    045-Directiva NgFor

--- 02-bases/src/app/heroes/list/list.component.ts
+++ 02-bases/src/app/heroes/list/list.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
+  public heroNames: string[] = ['SpiderMan', 'IronMan', 'She-Hulk', 'Thor']
 
 }
